65213f6e76aaa8cbaf442168880db757c2e12ae9
saadman <786saadman@gmail.com>
2020-04-05 12:57:42 +0600

    properly tested vue router

--- public/js/app.js
+++ public/js/app.js
@@ -1915,6 +1915,17 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   mounted: function mounted() {
     console.log('Component mounted.');
@@ -19523,8 +19534,30 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c(
-    "v-container",
-    [_c("v-alert", { attrs: { type: "info" } }, [_vm._v("I am Saad")])],
+    "div",
+    [
+      _c(
+        "v-container",
+        [
+          _c("v-alert", { attrs: { type: "info" } }, [_vm._v("I am Saad")]),
+          _vm._v(" "),
+          _c("v-btn", { attrs: { link: "", to: "/foo" } }, [
+            _vm._v("Load foo")
+          ]),
+          _vm._v(" "),
+          _c("v-btn", { attrs: { link: "", to: "/bar" } }, [
+            _vm._v("Load bar")
+          ]),
+          _vm._v(" "),
+          _c("v-btn", { attrs: { link: "", to: "/user/saad" } }, [
+            _vm._v("Load user")
+          ])
+        ],
+        1
+      ),
+      _vm._v(" "),
+      _c("v-content", [_c("router-view")], 1)
+    ],
     1
   )
 }
@@ -76304,8 +76337,26 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var vue_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! vue-router */ "./node_modules/vue-router/dist/vue-router.esm.js");
 
 
+var foo = {
+  template: "<v-alert type='error'>I am foo component</v-alert>"
+};
+var bar = {
+  template: "<v-alert type='error'>I am bar component</v-alert>"
+};
+var user = {
+  template: "<v-alert type='info'>I am {{$route.params.name}}</v-alert>"
+};
 vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]);
-var routes = [];
+var routes = [{
+  path: '/foo',
+  component: foo
+}, {
+  path: '/bar',
+  component: bar
+}, {
+  path: '/user/:name',
+  component: user
+}];
 /* harmony default export */ __webpack_exports__["default"] = (new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
   routes: routes
 }));
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,8 +1,19 @@
 <template>
+
+<div>
   <v-container>
     <v-alert type="info">I am Saad</v-alert>
+    <v-btn link to="/foo">Load foo</v-btn>
+    <v-btn link to="/bar">Load bar</v-btn>
+     <v-btn link to="/user/saad">Load user</v-btn>
+    
   </v-container>
-  
+
+    <v-content>
+        <router-view></router-view>
+    </v-content>
+
+</div>
 </template>
 
 <script>
